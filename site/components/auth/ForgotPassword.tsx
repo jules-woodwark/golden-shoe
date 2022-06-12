ef8503a0b0fd3aff949a85b05d7a79e8ae53e51e
Jules Woodwark <jules.woodwark@gmail.com>
2022-06-12 18:36:15 +0100

    fix logo props in Auth

--- site/components/auth/ForgotPassword.tsx
+++ site/components/auth/ForgotPassword.tsx
@@ -40,8 +40,8 @@ const ForgotPassword: FC<Props> = () => {
       onSubmit={handleResetPassword}
       className="w-80 flex flex-col justify-between p-3"
     >
-      <div className="flex justify-center pb-12 ">
-        <Logo width="64px" height="64px" />
+      <div className="flex justify-center pb-12">
+        <Logo />
       </div>
       <div className="flex flex-col space-y-4">
         {message && (
