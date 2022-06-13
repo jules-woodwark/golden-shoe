0a747d374bcc8705340b782d9f9cf497e0af5fac
Jules Woodwark <jules.woodwark@gmail.com>
2022-06-13 15:48:13 +0100

    redirect instead of 404

--- site/pages/_app.tsx
+++ site/pages/_app.tsx
@@ -3,13 +3,14 @@ import '@assets/chrome-bug.css'
 import 'keen-slider/keen-slider.min.css'
 
 import { FC, useEffect } from 'react'
+import App, { AppContext } from 'next/app'
 import type { AppProps } from 'next/app'
 import { Head } from '@components/common'
 import { ManagedUIContext } from '@components/ui/context'
 
 const Noop: FC = ({ children }) => <>{children}</>
 
-export default function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps }: AppProps) {
   const Layout = (Component as any).Layout || Noop
 
   useEffect(() => {
@@ -27,3 +28,17 @@ export default function MyApp({ Component, pageProps }: AppProps) {
     </>
   )
 }
+
+MyApp.getInitialProps = async (appContext: AppContext) => {
+  const appProps = await App.getInitialProps(appContext)
+
+  if (appContext.ctx.res?.statusCode === 404) {
+    appContext.ctx.res.writeHead(302, { Location: '/' })
+    appContext.ctx.res.end()
+    return
+  }
+
+  return { ...appProps }
+}
+
+export default MyApp
